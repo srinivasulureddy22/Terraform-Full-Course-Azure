537de0f29eef8783edfedb2c9fb6862db520d3ef
srinivasulureddy22 <61047715+srinivasulureddy22@users.noreply.github.com>
2025-09-10 18:25:15 +0530

    Update dev.sh

--- lessons/day26/scripts/dev.sh
+++ lessons/day26/scripts/dev.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 RESOURCE_GROUP_NAME=terraform-state-rg
-STAGE_SA_ACCOUNT=tfstagebackend2024piyush
-DEV_SA_ACCOUNT=tfdevbackend2024piyush
+STAGE_SA_ACCOUNT=tfstagetestingtw
+DEV_SA_ACCOUNT=terraformbackendtw
 CONTAINER_NAME=tfstate
 
 
